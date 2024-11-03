51b0357fa117992f218e20e9432d0f071732a853
Oana Spatacean <oana718@oana.spatacean>
2024-11-03 22:20:51 +0200

    made last changes for the first assignment

--- include/FrenchBulldog.h
+++ include/FrenchBulldog.h
@@ -10,38 +10,90 @@ class FrenchBulldog
         std::string name;
         bool snore;
         bool goofy; 
-        std::string *destr;
+        std::string *talk;
 
     public:
         //constructor pentru initializarea atributelor buldogului 
-        FrenchBulldog(const std::string &name, bool snore, bool goofy)
-            : name(name), snore(snore), goofy(goofy)  
+        FrenchBulldog(const std::string &name, bool snore, bool goofy) 
         {
-            destr = new std::string("Woof woof! I want to play!");
+            this->name = name;
+            this->snore = snore;
+            this->goofy = goofy;
+
+            this->talk = new std::string("Woof woof! I want to play!");
             std::cout<<"French bulldog is awake: "<<name<<std::endl;
         }
 
         //destructor pentru eliberarea memoriei de pe heap (memorie alocată dinamic)
         ~FrenchBulldog() 
         {
-            delete destr; 
+            delete talk; 
             std::cout<<"French bulldog feel asleep: "<<name<<std::endl;
         }
 
         //copy constructor pentru copierea atributelor și a resurselor alocate dinamic
         FrenchBulldog(const FrenchBulldog &exactCopyOfFrenchBulldog)
-            : name(exactCopyOfFrenchBulldog.name), snore(exactCopyOfFrenchBulldog.snore), goofy(exactCopyOfFrenchBulldog.goofy) 
-        {          
-            destr = new std::string(*(exactCopyOfFrenchBulldog.destr));
+        {   
+            this->name = exactCopyOfFrenchBulldog.name;
+            this->snore = exactCopyOfFrenchBulldog.snore;
+            this->goofy = exactCopyOfFrenchBulldog.goofy; 
+
+            talk = new std::string(*(exactCopyOfFrenchBulldog.talk));
+            
             std::cout<<"French bulldog copied (because it is so cute): "<<name<<std::endl;
+        }
+
+        //copy assignment operator
+        FrenchBulldog& operator=(const FrenchBulldog &exactCopyOfFrenchBulldog)
+        {
+            if (this == &exactCopyOfFrenchBulldog) {
+                return *this; 
+            }
+
+            delete talk; 
+
+            this->name = exactCopyOfFrenchBulldog.name;
+            this->snore = exactCopyOfFrenchBulldog.snore;
+            this->goofy = exactCopyOfFrenchBulldog.goofy;
+
+            talk = new std::string(*(exactCopyOfFrenchBulldog.talk)); 
+
+            std::cout<<"Copy assignment operator was called: "<<name<<std::endl;
+            return *this;
         }
 
         //move constructor pentru mutarea resurselor fără a crea duplicat
         FrenchBulldog(FrenchBulldog &&anotherFrenchBulldog) noexcept
-            : name(std::move(anotherFrenchBulldog.name)), snore(anotherFrenchBulldog.snore), goofy(anotherFrenchBulldog.goofy), destr(anotherFrenchBulldog.destr) 
         { 
-            anotherFrenchBulldog.destr = nullptr;
+            this->name = anotherFrenchBulldog.name;
+            this->snore = anotherFrenchBulldog.snore;
+            this->goofy = anotherFrenchBulldog.goofy;
+
+            this->talk = anotherFrenchBulldog.talk;
+            anotherFrenchBulldog.talk = nullptr;
+
             std::cout<<"French bulldog moved: "<<name<<std::endl;
+        }
+
+        //move assignment operator
+        FrenchBulldog& operator=(FrenchBulldog &&anotherFrenchBulldog) noexcept
+        {
+            if (this == &anotherFrenchBulldog) 
+            {
+                return *this; 
+            }
+
+            delete talk;
+
+            this->name = anotherFrenchBulldog.name;
+            this->snore = anotherFrenchBulldog.snore;
+            this->goofy = anotherFrenchBulldog.goofy;
+
+            this->talk = anotherFrenchBulldog.talk; 
+            anotherFrenchBulldog.talk = nullptr; 
+
+            std::cout<<"Move assignment operator was called: "<<name<<std::endl;
+            return *this;
         }
 
         void display() const 
@@ -66,7 +118,7 @@ class FrenchBulldog
                 std::cout<<" Goofy: It's just a sleepy little dog\n";
             }
 
-            std::cout<<name<<" says: "<<*destr<<std::endl;
+            std::cout<<name<<" says: "<<*talk<<std::endl;
 
             std::cout<<std::endl;
         }
--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,17 @@ int main() {
     bulldog2.display();
 
     //copiere folosind copy constructorul
-    FrenchBulldog bulldog3 = bulldog1;
+    FrenchBulldog bulldog3 = bulldog1; //Hera
     bulldog3.display();
 
+    bulldog1 = bulldog2; //copy assignment operator //Toby
+    bulldog1.display();
+
     //mutare folosind move constructorul
-    FrenchBulldog bulldog4 = std::move(bulldog2);
+    FrenchBulldog bulldog4 = std::move(bulldog2); //Toby
+    bulldog4.display();
+
+    bulldog2=std::move(bulldog3);//move assignment operator //Hera
     bulldog4.display();
 
     return 0;
